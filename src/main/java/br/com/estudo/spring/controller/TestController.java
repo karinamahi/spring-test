54c2b7b72784dc00faf0dd2520d03ff6016b86c5
Guilherme H. Bueno <guilhermehbueno@gmail.com>
2015-08-26 19:53:27 -0300

    Adicionado tag que faz o scan para injetar as dependencias do spring

--- src/main/java/br/com/estudo/spring/controller/TestController.java
+++ src/main/java/br/com/estudo/spring/controller/TestController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
